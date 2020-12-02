905330ad4f86575bef35f7af095959e3d6045a5e
viriditass <jiseungwoo0210@gmail.com>
2020-12-02 23:05:50 +0900

    add noise

--- image-artifact-generation/demo_image_artifact_generation.py
+++ image-artifact-generation/demo_image_artifact_generation.py
@@ -1,4 +1,9 @@
 import numpy as np
+from scipy.stats import poisson
+from scipy.io import loadmat
+
+from skimage.data import shepp_logan_phantom
+from skimage.transform import radon, iradon, rescale
 
 import matplotlib.pyplot as plt 
 
@@ -108,5 +113,113 @@ plt.title("Gaussian sampling mask")
 plt.subplot(133)
 plt.imshow(np.squeeze(dst), cmap=cmap, vmin=0, vmax=1)
 plt.title("Sampling image")
+
+plt.show()
+
+
+
+
+
+
+
+
+## Denoising
+
+## Denoising: Random noise
+sgm = 60.0
+
+noise = sgm/255.0 * np.random.randn(size[0], size[1], size[2])
+
+dst = img + noise
+
+plt.subplot(131)
+plt.imshow(np.squeeze(img), cmap=cmap, vmin=0, vmax=1)
+plt.title("Ground Truth")
+
+plt.subplot(132)
+plt.imshow(np.squeeze(noise), cmap=cmap, vmin=0, vmax=1)
+plt.title("Noise")
+
+plt.subplot(133)
+plt.imshow(np.squeeze(dst), cmap=cmap, vmin=0, vmax=1)
+plt.title("Noisy image")
+
+plt.show()
+
+## Denoising: Poisson noise (image-domain)
+dst = poisson.rvs(255.0 * img) / 255.0
+noise = dst - img
+
+plt.subplot(131)
+plt.imshow(np.squeeze(img), cmap=cmap, vmin=0, vmax=1)
+plt.title("Ground Truth")
+
+plt.subplot(132)
+plt.imshow(np.squeeze(noise), cmap=cmap, vmin=0, vmax=1)
+plt.title("Poisson Noise")
+
+plt.subplot(133)
+plt.imshow(np.squeeze(dst), cmap=cmap, vmin=0, vmax=1)
+plt.title("Noisy image")
+
+plt.show()
+
+## Denoising: Poisson noise (CT-domain)
+# system setting
+N = 512
+ANG = 180
+VIEW = 360
+THETA = np.linspace(0, ANG, VIEW, endpoint=False)
+
+A = lambda x: radon(x, THETA, circle=False).astype(np.float32)
+AT = lambda y: iradon(y, THETA, circle=False, filter=None, output_size=N).astype(np.float32)
+AINV = lambda y: iradon(y, THETA, circle=False, output_size=N).astype(np.float32)
+
+# Low dose CT: adding poisson noise
+pht = shepp_logan_phantom()
+pht = 0.03 * rescale(pht, scale=512/400, order=0)
+
+prj = A(pht)
+
+i0 = 1e4
+dst = np.exp(-prj)
+dst = poisson.rvs(i0 * dst)
+dst[dst < 1] = 1
+dst = -np.log(dst / i0)
+dst[dst < 0] = 0
+
+noise = dst - prj
+
+rec = AINV(prj)
+rec_noise = AINV(noise)
+rec_dst = AINV(dst)
+
+plt.subplot(241)
+plt.imshow(pht, cmap='gray', vmin=0, vmax=0.03)
+plt.title("Ground Truth")
+
+plt.subplot(242)
+plt.imshow(rec, cmap="gray", vmin=0, vmax=0.03)
+plt.title("Reconstruction")
+
+plt.subplot(243)
+plt.imshow(rec_noise, cmap="gray")
+plt.title("Reconstruction using Noise")
+
+plt.subplot(244)
+plt.imshow(rec_dst, cmap="gray", vmin=0, vmax=0.03)
+plt.title("Reconstruction using Noisy data")
+
+plt.subplot(246)
+plt.imshow(prj, cmap="gray")
+plt.title("Projection data")
+
+plt.subplot(247)
+plt.imshow(noise, cmap="gray")
+plt.title("Poisson Noise in projection")
+
+plt.subplot(248)
+plt.imshow(dst, cmap="gray")
+plt.title("Noisy data")
 
 plt.show()
