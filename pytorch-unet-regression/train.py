a7d6207a4b3f4442cb57fe18d9d6ee469c03cfbb
viriditass <jiseungwoo0210@gmail.com>
2020-12-09 14:08:29 +0900

    change tensorboard save

--- pytorch-unet-regression/train.py
+++ pytorch-unet-regression/train.py
@@ -176,13 +176,24 @@ if mode == 'train':
                   (epoch, num_epoch, batch, num_batch_train, np.mean(loss_arr)))
 
             # Tensorboard save
-            label = fn_tonumpy(label)
+            label = fn_tonumpy(fn_denorm(label, mean=0.5, std=0.5))
             input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
-            output = fn_tonumpy(fn_class(output))
+            output = fn_tonumpy(fn_denorm(output, mean=0.5, std=0.5))
 
-            writer_train.add_image('label', label, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
-            writer_train.add_image('input', input, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
-            writer_train.add_image('output', output, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+            """
+            writer_val.add_image('label', label, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+            writer_val.add_image('input', input, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+            writer_val.add_image('output', output, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+            """
+
+            input = np.clip(input, a_min=0, a_max=1)
+            output = np.clip(output, a_min=0, a_max=1)
+
+            id = num_batch_train * (epoch - 1) + batch
+
+            plt.imsave(os.path.join(result_dir_train, 'png', '%04d_label.png' % id), label[0])
+            plt.imsave(os.path.join(result_dir_train, 'png', '%04d_input.png' % id), input[0])
+            plt.imsave(os.path.join(result_dir_train, 'png', '%04d_output.png' % id), output[0])
 
         writer_train.add_scalar('loss', np.mean(loss_arr), epoch)
 
@@ -206,13 +217,27 @@ if mode == 'train':
                       (epoch, num_epoch, batch, num_batch_val, np.mean(loss_arr)))
 
                 # Tensorboard save
-                label = fn_tonumpy(label)
+                label = fn_tonumpy(fn_denorm(label, mean=0.5, std=0.5))
                 input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
-                output = fn_tonumpy(fn_class(output))
-
+                output = fn_tonumpy(fn_denorm(output, mean=0.5, std=0.5))
+                
+                """
                 writer_val.add_image('label', label, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
                 writer_val.add_image('input', input, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
                 writer_val.add_image('output', output, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+                """
+
+                input = np.clip(input, a_min=0, a_max=1)
+                output = np.clip(output, a_min=0, a_max=1)
+
+                id = num_batch_train * (epoch - 1) + batch
+
+                plt.imsave(os.path.join(result_dir_train, 'png', '%04d_label.png' % id), label[0])
+                plt.imsave(os.path.join(result_dir_train, 'png', '%04d_input.png' % id), input[0])
+                plt.imsave(os.path.join(result_dir_train, 'png', '%04d_output.png' % id), output[0])
+
+
+
 
         writer_val.add_scalar('loss', np.mean(loss_arr), epoch)
 
@@ -246,20 +271,30 @@ else:
                   (batch, num_batch_test, np.mean(loss_arr)))
 
             # Tensorboard save
-            label = fn_tonumpy(label)
+            label = fn_tonumpy(fn_denorm(label, mean=0.5, std=0.5))
             input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
-            output = fn_tonumpy(fn_class(output))
+            output = fn_tonumpy(fn_denorm(output, mean=0.5, std=0.5))
 
             for j in range(label.shape[0]):
                 id = num_batch_test * (batch - 1) + j
 
-                plt.imsave(os.path.join(result_dir, 'png', 'label_%04d.png' % id), label[j].squeeze(), cmap='gray')
-                plt.imsave(os.path.join(result_dir, 'png', 'input_%04d.png' % id), input[j].squeeze(), cmap='gray')
-                plt.imsave(os.path.join(result_dir, 'png', 'output_%04d.png' % id), output[j].squeeze(), cmap='gray')
+                label_ = label[j]
+                input_ = input[j]
+                output_ = output[j]
+                
+                np.save(os.path.join(result_dir, 'numpy', '%04d_label.npy' % id), label_)
+                np.save(os.path.join(result_dir, 'numpy', '%04d_input.npy' % id), input_)
+                np.save(os.path.join(result_dir, 'numpy', '%04d_output.npy' % id), output_)
 
-                np.save(os.path.join(result_dir, 'numpy', 'label_%04d.npy' % id), label[j].squeeze())
-                np.save(os.path.join(result_dir, 'numpy', 'input_%04d.npy' % id), input[j].squeeze())
-                np.save(os.path.join(result_dir, 'numpy', 'output_%04d.npy' % id), output[j].squeeze())
+                label_ = np.clip(label_, a_min=0, a_max=1)
+                input_ = np.clip(input_, a_min=0, a_max=1)
+                output_ = np.clip(output_, a_min=0, a_max=1)
 
+
+                plt.imsave(os.path.join(result_dir, 'png', '%04d_label.npy' % id), label_)
+                plt.imsave(os.path.join(result_dir, 'png', '%04d_input.npy' % id), input_)
+                plt.imsave(os.path.join(result_dir, 'png', '%04d_output.npy' % id), output_)
+
+                
     print("AVERAGE TEST: BATCH %04d / %04d | LOSS %.4f" %
           (batch, num_batch_test, np.mean(loss_arr)))
