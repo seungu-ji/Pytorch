4b429ac74d2d69a1808600fbfbadfd3345c01944
viriditass <jiseungwoo0210@gmail.com>
2020-12-10 18:04:04 +0900

    change data_loader

--- pytorch-unet-regression/data_loader.py
+++ pytorch-unet-regression/data_loader.py
@@ -1,10 +1,10 @@
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
 
+import matplotlib.pyplot as plt
 from util import *
 
 ## Data Loader
@@ -15,12 +15,12 @@ class Dataset(torch.utils.data.Dataset):
         self.task = task
         self.opts = opts
 
-        lst_data = os.listdir(self.data_dir)
+        self.to_tensor = ToTensor()
 
-        lst_data = [f for f in lst_data if f.endswith('jpg') | f.endswith('png')]
+        lst_data = os.listdir(self.data_dir)
+        lst_data = [f for f in lst_data if f.endswith('jpg') | f.endswith('jpeg') | f.endswith('png')]
 
         lst_data.sort()
-
         self.lst_data = lst_data
 
     def __len__(self):
@@ -29,32 +29,33 @@ class Dataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         # label = np.load(os.path.join(self.data_dir, self.lst_label[index]))
         # input = np.load(os.path.join(self.data_dir, self.lst_input[index]))
-        img = plt.imread(os.path.join(self.data_dir, self.lst_data[index]))
-        size = img.shape
 
-        if size[0] > size[1]: # 항상 가로로 긴 이미지로 저장
+        img = plt.imread(os.path.join(self.data_dir, self.lst_data[index]))
+        sz = img.shape
+
+        if sz[0] > sz[1]:
             img = img.transpose((1, 0, 2))
 
-        if img.dtype == np.uint8:
-            img = img/255.0
-
         if img.ndim == 2:
-            label = label[:, :, np.newaxis]
+            img = img[:, :, np.newaxis]
 
-        label = img
+        if img.dtype == np.uint8:
+            img = img / 255.0
 
-        if self.task == "denoising":
-            input = add_noise(img, type=self.opts[0], opts=self.opts[1])
-        elif self.task == "inpainting":
-            input = add_sampling(img, type=self.opts[0], opts=self.opts[1])
-        elif self.task == "super_resolution":
-            input = add_blur(img, type=self.opts[0], opts=self.opts[1])
+        data = {'label': img}
 
-
-        data = {'input': input, 'label': label}
+        if self.task == "inpainting":
+            data['input'] = add_sampling(data['label'], type=self.opts[0], opts=self.opts[1])
+        elif self.task == "denoising":
+            data['input'] = add_noise(data['label'], type=self.opts[0], opts=self.opts[1])
 
         if self.transform:
             data = self.transform(data)
+
+        if self.task == "super_resolution":
+            data['input'] = add_blur(data['label'], type=self.opts[0], opts=self.opts[1])
+
+        data = self.to_tensor(data)
 
         return data
 
@@ -62,14 +63,18 @@ class Dataset(torch.utils.data.Dataset):
 # ToTensor(): numpy -> tensor
 class ToTensor(object):
     def __call__(self, data):
+        """
         label, input = data['label'], data['input']
-
         # Image의 numpy 차원 = (Y, X, CH)
         # Image의 tensor 차원 = (CH, Y, X)
         label = label.transpose((2, 0, 1)).astype(np.float32)
         input = input.transpose((2, 0, 1)).astype(np.float32)
-
         data = {'label': torch.from_numpy(label), 'input': torch.from_numpy(input)}
+        """
+        
+        for key, value in data.items():
+            value = value.transpose((2, 0, 1)).astype(np.float32)
+            data[key] = torch.from_numpy(value)
 
         return data
 
@@ -79,52 +84,65 @@ class Normalization(object):
         self.std = std
 
     def __call__(self, data):
+        """
         label, input = data['label'], data['input']
-        
-        # regression은 label에도 적용해줘야함
-        label = (label - self.mean) / self.std
         input = (input - self.mean) / self.std
-
+        label = (label - self.mean) / self.std
+        # regression은 label에도 적용해줘야함
         data = {'label': label, 'input': input}
+        """
+
+        for key, value in data.items():
+            data[key] = (value - self.mean) / self.std
 
         return data
 
+
 class RandomFlip(object):
     def __call__(self, data):
-        label, input = data['label'], data['input']
+        # label, input = data['label'], data['input']
 
         if np.random.rand() > 0.5:
-            label = np.fliplr(label)
-            input = np.fliplr(input)
+            # label = np.fliplr(label)
+            # input = np.fliplr(input)
+
+            for key, value in data.items():
+                data[key] = np.flip(value, axis=0)
 
         if np.random.rand() > 0.5:
-            label = np.flipud(label)
-            input = np.flipud(input)
+            # label = np.flipud(label)
+            # input = np.flipud(input)
 
-        data = {'label': label, 'input': input}
+            for key, value in data.items():
+                data[key] = np.flip(value, axis=1)
+
+        # data = {'label': label, 'input': input}
 
         return data
 
 
 class RandomCrop(object):
-    def __init__(self, shape):
-        self.shape = shape
+  def __init__(self, shape):
+      self.shape = shape
 
-    def __call__(self, data):
-        input, label = data['input'], data['label']
+  def __call__(self, data):
+    # input, label = data['input'], data['label']
+    # h, w = input.shape[:2]
 
-        h, w = input.shape[:2]
-        new_h, new_w = self.shape
+    h, w = data['label'].shape[:2]
+    new_h, new_w = self.shape
 
-        top = np.random.randint(0, h - new_h)
-        left = np.random.randint(0, w - new_w)
+    top = np.random.randint(0, h - new_h)
+    left = np.random.randint(0, w - new_w)
 
-        id_y = np.arange(top, top + new_h, 1)[:, np.newaxis]
-        id_x = np.arange(left, left + new_w, 1)
+    id_y = np.arange(top, top + new_h, 1)[:, np.newaxis]
+    id_x = np.arange(left, left + new_w, 1)
 
-        input = input[id_y, id_x]
-        label = label[id_y, id_x]
+    # input = input[id_y, id_x]
+    # label = label[id_y, id_x]
+    # data = {'label': label, 'input': input}
 
-        data = {'input': input, 'label': label}
+    for key, value in data.items():
+        data[key] = value[id_y, id_x]
 
-        return data
+    return data
